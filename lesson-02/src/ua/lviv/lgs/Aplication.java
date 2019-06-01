dbb9499780a49006baba0ad1c18c300c7bba5485
LogosITAcademy <yana@192.168.60.103>
2019-06-01 21:21:47 +0300

    added first project to github

---
+++ lesson-02/src/ua/lviv/lgs/Aplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs;
+
+public class Aplication {
+	public static void main(String[] args) {
+		int a = 2;
+		long b = 3;
+		
+		System.out.printIn("var a = " + a);
+		System.out.printIn("var b = " + b);
+		
+	}
+
+	}
